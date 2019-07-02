e5ae08364f709904dd86150164beb39bcfad9f1a
j.berthel <j.berthel@codeblick.de>
2019-07-02 16:13:50 +0200

    set doc root via env variable

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-sed -ri -e "s!/var/www/html!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/sites-available/*.conf
-sed -ri -e "s!/var/www/!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+FILE=/etc/apache2/apdoro.txt
+
+if [ -f "$FILE" ]; then
+    echo "APACHE DOCUMENT ROOT already set up!"
+else
+    sed -ri -e "s!/var/www/html!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/sites-available/*.conf
+    sed -ri -e "s!/var/www/!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+    touch $FILE
+fi
+
+
 
 exec apache2-foreground
